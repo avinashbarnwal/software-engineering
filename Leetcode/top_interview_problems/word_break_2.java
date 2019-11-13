0f1e29402431da7b01fd94967002a316d40de1fe
Avinash Barnwal <avinashbarnwal123@gmail.com>
2019-11-13 14:08:06 -0500

    changes to practice code

---
+++ Leetcode/top_interview_problems/word_break_2.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public List<String> wordBreak(String s, Set<String> wordDict) {
+        return word_Break(s, wordDict, 0);
+    }
+    public List<String> word_Break(String s, Set<String> wordDict, int start) {
+        LinkedList<String> res = new LinkedList<>();
+        if (start == s.length()) {
+            res.add("");
+        }
+        for (int end = start + 1; end <= s.length(); end++) {
+            if (wordDict.contains(s.substring(start, end))) {
+                List<String> list = word_Break(s, wordDict, end);
+                for (String l : list) {
+                    res.add(s.substring(start, end) + (l.equals("") ? "" : " ") + l);
+                }
+            }
+        }
+        return res;
+    }
+}
---
+++ Machine_learning_Systems/Notes _ Markdown Notes_ fast markdown editor with LaTeX support_files/app-config.js
@@ -0,0 +1,1 @@
+angular.module('notes.config').constant('DEMO_MODE', false);
